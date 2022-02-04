46e2fc43ccd5a15c2b88d6dc6b13aa3031a07df1
pmfoysal <pmfoysal@gmail.com>
2022-02-04 12:07:42 +0600

    numToGrade.js - Solving Method 06 Added

--- numToGrade.js
+++ numToGrade.js
@@ -133,3 +133,33 @@ numbers.map(mark => {
    }
    i++;
 });
+
+// Solving Method 06 - Using Function with Array
+console.log('\n==== Solving Method 06 - Using Function with Array ====');
+
+var numbers = [95, 66, 80, 59, 47, 77];
+var names = ['Alia', 'Dalia', 'Salia', 'Malia', 'Lilia', 'Jalaia'];
+
+for (var i = 0; i < names.length; i++) {
+   var name = names[i];
+   var mark = numbers[i];
+   numToGradeOf(name, mark);
+}
+
+function numToGradeOf(name, mark) {
+   if (mark > 0 && mark <= 100) {
+      if (mark >= 80) {
+         console.log(name + ' has got A+');
+      } else if (mark >= 70) {
+         console.log(name + ' has got A');
+      } else if (mark >= 60) {
+         console.log(name + ' has got A-');
+      } else if (mark >= 50) {
+         console.log(name + ' has got B');
+      } else {
+         console.log(name + ' has Failed!');
+      }
+   } else {
+      console.log('Invalid Numbers Found!');
+   }
+}
